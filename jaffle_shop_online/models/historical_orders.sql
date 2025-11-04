49600c055997347ed12cc256e3dd86549158c28a
Joost Boonzajer Flaes <joostboon@gmail.com>
2025-11-04 14:16:40 +0200

    removed column

--- jaffle_shop_online/models/historical_orders.sql
+++ jaffle_shop_online/models/historical_orders.sql
@@ -28,7 +28,6 @@ final as (
         o.order_id,
         o.customer_id,
         o.order_date,
-        o.status,
         {% for payment_method in payment_methods -%}
         op.{{ payment_method }}_amount,
         {% endfor -%}
