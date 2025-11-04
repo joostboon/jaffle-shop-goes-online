f1293c9358abb19978f934a92808512f71fa0025
Joost Boonzajer Flaes <joostboon@gmail.com>
2025-11-04 13:00:12 +0200

    removed column

--- jaffle_shop_online/models/customers.sql
+++ jaffle_shop_online/models/customers.sql
@@ -58,8 +58,7 @@ final as (
         customers.last_name,
         customer_orders.first_order,
         customer_orders.most_recent_order,
-        customer_orders.number_of_orders,
-        customer_payments.total_amount as customer_lifetime_value
+        customer_orders.number_of_orders
 
     from customers
 
