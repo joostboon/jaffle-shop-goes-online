b79a5ed364f3e49a61c91577f2ad3c19489327b3
Joost Boonzajer Flaes <joostboon@gmail.com>
2025-11-04 15:57:13 +0200

    removing column

--- jaffle_shop_online/models/historical_orders.sql
+++ jaffle_shop_online/models/historical_orders.sql
@@ -18,7 +18,6 @@ order_payments as (
         {% for payment_method in payment_methods -%}
         sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
         {% endfor -%}
-        sum(amount) as total_amount
     from payments
     group by order_id
 ),
